f0ed694bb0bdaff8bce689c640330aaf6b2527ee
Ronald Harris <rharris47west@gmail.com>
2025-08-25 21:55:37 -0700

    Add greet.py script

---
+++ greet.py
@@ -0,0 +1,10 @@
+greet = "Hello, World!"
+print(greet)
+
+print("Start: ", greet[0:3])
+print("Middle: ", greet[3:6])
+print("End: ", greet[-3])
+
+a = greet.find(",")
+
+print("Portion before comma", greet[:a])
